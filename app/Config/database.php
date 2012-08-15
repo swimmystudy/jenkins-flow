fea13df35797e172d8a5e07769223a0480d71f50
ootatter <ohta@milds.net>
2012-08-15 23:12:57 +0900

    sqlite3

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,23 +1,33 @@
 <?php
 class DATABASE_CONFIG {
 
+
 	public $default = array(
-		'datasource' => 'Database/Mysql',
+		'datasource' => 'Database/Sqlite',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'jenkinsflow',
-		'password' => 'jenkinsflow',
-		'database' => 'jenkinsflow',
+		'login' => '',
+		'password' => '',
+		'database' => 'db/database.sqlite3',
 		'prefix' => 'blog_',
 	);
 
 	public $test = array(
-		'datasource' => 'Database/Mysql',
+		'datasource' => 'Database/Sqlite',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'jenkinsflow',
-		'password' => 'jenkinsflow',
-		'database' => 'jenkinsflow_test',
+		'login' => '',
+		'password' => '',
+		'database' => 'db/database_test.sqlite3',
 		'prefix' => 'blog_',
-	);	
+	);
+/**
+ * フルパスでないと駄目みたい。
+ *
+ */
+	public function __construct(){
+		$this->default['database'] = APP.$this->default['database'];
+		$this->test['database'] = APP.$this->test['database'];
+	}
+
 }
--- app/Test/bootstrap.php
+++ app/Test/bootstrap.php
@@ -1,2 +1,3 @@
 <?php
+ini_set('include_path', ROOT . DS . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
 Configure::write('debug', 0);
